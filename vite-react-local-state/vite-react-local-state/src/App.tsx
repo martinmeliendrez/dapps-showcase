080cb20ecfdf4d7f4217f68c34b487fa0837aff4
Martin Meliendrez <martinmeliendrez@gmail.com>
2024-09-21 08:02:17 -0300

    Created connect buttons
    Created an array of buttons that the user can select to connect to the EIP-6963 wallet providers that you detect.

--- vite-react-local-state/vite-react-local-state/src/App.tsx
+++ vite-react-local-state/vite-react-local-state/src/App.tsx
@@ -1,9 +1,37 @@
+import { useSyncProviders } from "./hooks/useSyncProviders"
 import "./App.css"
 
 const App = () => {
+  const providers = useSyncProviders()
+
+  const handleConnect = async (providerWithInfo: EIP6963ProviderDetail) => {
+    try {
+      const accounts = (await providerWithInfo.provider.request({
+        method: "eth_requestAccounts",
+      })) as string[]
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="App">
       <h2>Wallets Detected:</h2>
+      <div className="providers">
+        {providers.length > 0 ? (
+          providers?.map((provider: EIP6963ProviderDetail) => (
+            <button
+              key={provider.info.uuid}
+              onClick={() => handleConnect(provider)}
+            >
+              <img src={provider.info.icon} alt={provider.info.name} />
+              <div>{provider.info.name}</div>
+            </button>
+          ))
+        ) : (
+          <div>No Announced Wallet Providers</div>
+        )}
+      </div>
     </div>
   )
 }
