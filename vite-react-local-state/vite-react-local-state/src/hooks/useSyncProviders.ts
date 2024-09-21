7aca9e90ceb43a2fb14e7ad402787d6ad2ff609c
Martin Meliendrez <martinmeliendrez@gmail.com>
2024-09-21 07:59:06 -0300

    Added comments about useSyncExternalStore arguments

--- vite-react-local-state/vite-react-local-state/src/hooks/useSyncProviders.ts
+++ vite-react-local-state/vite-react-local-state/src/hooks/useSyncProviders.ts
@@ -1,5 +1,10 @@
 import { useSyncExternalStore } from "react"
 import { store } from "./store"
 
+// useSyncExternalStore takes three arguments:
+// - A subscription function to listen for changes in the external store (store.subscribe).
+// - A function to get the current value of the store (store.value).
+// - An initial value for the store (store.value).
+
 export const useSyncProviders = () =>
   useSyncExternalStore(store.subscribe, store.value, store.value)
